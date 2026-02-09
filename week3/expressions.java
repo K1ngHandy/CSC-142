924f7efc107e58c3c0411d9d1a3e0c282c3afb63
Stephen Handy <K1ngHandy@users.noreply.github.com>
2026-02-09 16:56:06 -0500

    correct expressions with strings

--- week3/expressions.java
+++ week3/expressions.java
@@ -2,11 +2,17 @@ package week3;
 
 public class expressions {
     public static void main(String[] args) {
-        String str1 = 2 + 6 + "cs";
-        String str2 = "cs 123" + 2 + 6;
-        double dbl1 = 1 + 9 / 2 * 2.0;
-        double dbl2 = 46 / 3 / 2.0 / 3 * 4 / 5;
-        double dbl3 = 50 / 9 / 2.0 + 200 / 10 / (5.0 / 2);
+        // String str1 = 2 + 6 + "cs";
+        // String str2 = "cs 123" + 2 + 6;
+        // double dbl1 = 1 + 9 / 2 * 2.0;
+        // double dbl2 = 46 / 3 / 2.0 / 3 * 4 / 5;
+        // double dbl3 = 50 / 9 / 2.0 + 200 / 10 / (5.0 / 2);
+
+        // System.out.println(str1);
+        // System.out.println(str2);
+        // System.out.println(dbl1);
+        // System.out.println(dbl2);
+        // System.out.println(dbl3);
 
         String str3 = "hello" + 42;
         String str4 = 1 + "abc" + 2;
@@ -18,20 +24,14 @@ public class expressions {
         String str10 = 1 + 1 + 1 + "1" + 1 + 1 + 1;
         String str11 = 4 - 1 + "abc";
 
-        System.out.println(str1);
-        System.out.println(str2);
-        System.out.println(dbl1);
-        System.out.println(dbl2);
-        System.out.println(dbl3);
-
-        // System.out.println(str3);
-        // System.out.println(str4);
-        // System.out.println(str5);
-        // System.out.println(str6);
-        // System.out.println(str7);
-        // System.out.println(str8);
-        // System.out.println(str9);
-        // System.out.println(str10);
-        // System.out.println(str11);
+        System.out.println(str3);
+        System.out.println(str4);
+        System.out.println(str5);
+        System.out.println(str6);
+        System.out.println(str7);
+        System.out.println(str8);
+        System.out.println(str9);
+        System.out.println(str10);
+        System.out.println(str11);
     }
 }
