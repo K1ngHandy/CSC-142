7cf87c8d1432d2d5fdb5de18e12fba603738cd9a
Stephen Handy <K1ngHandy@users.noreply.github.com>
2026-02-19 09:32:53 -0500

    A3, question 1

---
+++ week5/A3.java
@@ -0,0 +1,15 @@
+package week5;
+
+public class A3 {
+    public static void main(String[] args) {
+        // timesOperator
+        double y;
+        double x = 3;
+        // y = 12.3x^4 - 9.1x^3 + 19.3x^2 - 4.6x + 34.2
+        // y =
+
+        y = (12.3 * Math.pow(x, 4)) - (9.1 * Math.pow(x, 3)) + (19.3 * Math.pow(x, 2)) - (4.6 * x) + 34.2;
+
+        System.out.println(y);
+    }
+}
