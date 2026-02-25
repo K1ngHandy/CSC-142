6544fad82350a7c1106c5c5d858e01600af4f787
Stephen Handy <K1ngHandy@users.noreply.github.com>
2026-02-25 18:11:12 -0500

    add mirror scaffolding

---
+++ week6/Mirror.java
@@ -0,0 +1,26 @@
+package week6;
+
+public class Mirror {
+    // field variable size is accessible to all methods in this class
+    // final means that the value of size cannot be changed after initialized
+    public static final int SIZE = 40;
+
+    public static void main(String[] args) {
+        line();
+        topHalf();
+        bottomHalf();
+        line();
+    }
+
+    public static void topHalf() {
+        //
+    }
+
+    public static void bottomHalf() {
+        //
+    }
+
+    public static void line() {
+        // top and bottom lines
+    }
+}
