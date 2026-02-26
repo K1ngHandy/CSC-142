d6597ca2661201805df88d619e1a6591cde9c60a
Stephen Handy <K1ngHandy@users.noreply.github.com>
2026-02-26 15:13:08 -0500

    add constant size

--- week6/AsciiFigure.java
+++ week6/AsciiFigure.java
@@ -1,8 +1,10 @@
 package week6;
 
 public class AsciiFigure {
+    public static final int SIZE = 5;
+
     public static void main(String[] args) {
-        for (int i = 1; i <= 5; i++) {
+        for (int i = 1; i <= SIZE; i++) {
             for (int j = 1; j <= -4 * i + 20; j++) {
                 System.out.print("/");
             }
