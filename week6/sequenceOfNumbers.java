6c2982c6abb3859236cd4ffc30595e9e3eca3bed
Stephen Handy <K1ngHandy@users.noreply.github.com>
2026-03-01 10:13:11 -0500

    num sequence

---
+++ week6/sequenceOfNumbers.java
@@ -0,0 +1,12 @@
+package week6;
+
+public class sequenceOfNumbers {
+    public static void main(String[] args) {
+        int num = 8;
+
+        for (int i = 1; i <= 6; i++) {
+            System.out.println(num);
+            num += 3;
+        }
+    }
+}
