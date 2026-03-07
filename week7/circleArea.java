739dfb8c977806408ef0ce93378b6a9c7dd8df07
Stephen Handy <K1ngHandy@users.noreply.github.com>
2026-03-07 11:44:06 -0500

    circle radius

--- week7/circleArea.java
+++ week7/circleArea.java
@@ -1,5 +1,16 @@
 package week7;
 
 public class circleArea {
-    
+    public static final double RADIUS = 2.0;
+
+    public static void main(String[] args) {
+        double res = circleArea(RADIUS);
+        System.out.print(res);
+    }
+
+    public static double circleArea(double RADIUS) {
+        // area = pi * r ^ 2
+        double area = Math.PI * Math.pow(RADIUS, 2);
+        return area;
+    }
 }
