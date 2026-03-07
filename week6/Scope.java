ff60224469e630cf52282e9b7bb5c272c2937471
Stephen Handy <K1ngHandy@users.noreply.github.com>
2026-03-06 23:20:11 -0500

    stars box

--- week6/Scope.java
+++ week6/Scope.java
@@ -2,19 +2,6 @@ package week6;
 
 public class Scope {
     public static void main(String[] args) {
-        // for (int i = 0; i < 10; i++) {
-        // System.out.println("x");
-
-        // for (int j = 0; j < i; j++) {
-        // System.out.println("y");
-        // }
-
-        // System.out.println("z");
-
-        // for (int k = 0; k < i; k++) {
-        // System.out.println("w");
-        // }
-        // }
         printNum(4, 9);
         printNum(17, 6);
         printNum(8, 0);
--- week7/boxOfStars.java
+++ week7/boxOfStars.java
@@ -1,5 +1,33 @@
 package week7;
 
 public class boxOfStars {
-    
+    public static final int WIDTH = 8;
+    public static final int HEIGHT = 5;
+
+    public static void main(String[] args) {
+        boxOfStars(WIDTH, HEIGHT);
+    }
+
+    public static void boxOfStars(int WIDTH, int HEIGHT) {
+        starsRow();
+        centerRows();
+        starsRow();
+    }
+
+    public static void starsRow() {
+        for (int i = 1; i <= WIDTH; i++) {
+            System.out.print("*");
+        }
+        System.out.println();
+    }
+
+    public static void centerRows() {
+        for (int j = 1; j <= HEIGHT - 2; j++) {
+            System.out.print("*");
+            for (int k = 1; k <= WIDTH - 2; k++) {
+                System.out.print(" ");
+            }
+            System.out.println("*");
+        }
+    }
 }
