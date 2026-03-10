6bbe0c0d7bf707c5630da14ff474082d396b1e83
Stephen Handy <K1ngHandy@users.noreply.github.com>
2026-03-10 00:49:10 -0400

    factorial

--- week7/factorial.java
+++ week7/factorial.java
@@ -1,5 +1,17 @@
 package week7;
 
-public class factorial {
+public class Factorial {
+    public static final int N = 4;
 
+    public static void main(String[] args) {
+        System.out.println(factorial(N));
+    }
+
+    public static int factorial(int N) {
+        int res = 1;
+        for (int i = 1; i <= N; i++) {
+            res *= i;
+        }
+        return res;
+    }
 }
