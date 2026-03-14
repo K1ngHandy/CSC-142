5750e46214cb6ab72d4bdc2b418d78921e253b4a
Stephen Handy <K1ngHandy@users.noreply.github.com>
2026-03-14 18:21:33 -0400

    add param mystery 3

--- week7/ParameterMysteryExam3.java
+++ week7/ParameterMysteryExam3.java
@@ -1,5 +1,23 @@
 package week7;
 
 public class ParameterMysteryExam3 {
-    
+    public static void main(String[] args) {
+        int a = 5;
+        int b = 1;
+        int c = 3;
+        int three = a;
+        int one = b + 1;
+
+        axiom(a, b, c);
+        axiom(c, three, 10);
+        three = axiom(b + c, one + three, a + one);
+        a++;
+        b = 0;
+        a = axiom(three, 2, one);
+    }
+
+    public static int axiom(int c, int b, int a) {
+        System.out.println(a + " + " + c + " = " + b);
+        return a + b;
+    }
 }
