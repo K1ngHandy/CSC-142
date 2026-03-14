d5d6991dda87aaa3a1aafaf8287009991362658b
Stephen Handy <K1ngHandy@users.noreply.github.com>
2026-03-14 18:19:41 -0400

    add parameter mystery

--- week7/ParameterMystery1.java
+++ week7/ParameterMystery1.java
@@ -1,5 +1,16 @@
 package week7;
 
 public class ParameterMystery1 {
-    
+    public static void main(String[] args) {
+        int x = 9;
+        int y = 2;
+        int z = 5;
+
+        mystery(z, y, x);
+        mystery(y, x, z);
+    }
+
+    public static void mystery(int x, int z, int y) {
+        System.out.println(z + ", " + (y - x));
+    }
 }
