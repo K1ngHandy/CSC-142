9452c6e2f225f11d3393b596da391dceda42019b
Stephen Handy <K1ngHandy@users.noreply.github.com>
2026-03-15 16:31:49 -0400

    debug one two three

--- week7/OneTwoThree.java
+++ week7/OneTwoThree.java
@@ -6,28 +6,28 @@ public class OneTwoThree {
         addOne();
         addTwo(a);
         a = addThreeAndReturnResult(a);
-        println("run: a = " + a + ", b = " + b);
+        System.out.println("run: a = " + a + ", b = " + b);
     }
 
     private static void addOne() {
         int a = 101;
         b++;
-        println("addOne: a = " + a + ", b = " + b);
+        System.out.println("addOne: a = " + a + ", b = " + b);
     }
 
     private static void addTwo(int a) {
         a += 2;
         b += 2;
-        println("addTwo: a = " + a + ", b = " + b);
+        System.out.println("addTwo: a = " + a + ", b = " + b);
     }
 
     private static int addThreeAndReturnResult(int a) {
         a += 3;
         b += 3;
-        println("addThreeAndReturnResult: a = " + a + ", b = " + b);
+        System.out.println("addThreeAndReturnResult: a = " + a + ", b = " + b);
         return a;
     }
 
     /* Private instance variable */
-    private int b = 200;
+    private static int b = 200;
 }
--- week7/printPay.java
+++ week7/printPay.java
@@ -1,5 +1,15 @@
 package week7;
 
-public class printPay {
-    
+public class PrintPay {
+    public static final double SALARY = 10.0;
+    public static final int HOURS = 40;
+
+    public static void main(String[] args) {
+        printPay(SALARY, HOURS);
+    }
+
+    public static int printPay(double salary, int hours) {
+        System.out.println("Hours worked: " + hours);
+        System.out.print("Pay Earned: " + salary * hours);
+    }
 }
