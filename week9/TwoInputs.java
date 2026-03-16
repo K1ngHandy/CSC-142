d46ba4d5971b7e23f7f28ee32c5262be6b42740c
Stephen Handy <K1ngHandy@users.noreply.github.com>
2026-03-16 18:11:24 -0400

    input product example

---
+++ week9/TwoInputs.java
@@ -0,0 +1,16 @@
+package week9;
+
+import java.util.Scanner;
+
+public class TwoInputs {
+    // Input 2 nums and calculate product
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Please type 2 numbers: ");
+        int x = sc.nextInt();
+        int y = sc.nextInt();
+
+        int product = x * y;
+        System.out.println("The product is " + product);
+    }
+}
