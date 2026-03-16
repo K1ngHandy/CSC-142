3e3feaeb2cb26a81c5068ea289c389478feb3500
Stephen Handy <K1ngHandy@users.noreply.github.com>
2026-03-16 00:06:37 -0400

    add sum of range

--- week7/sumOfRange.java
+++ week7/sumOfRange.java
@@ -1,5 +1,25 @@
 package week7;
 
-public class sumOfRange {
-    
+public class SumOfRange {
+    public static final int MIN = 3;
+    public static final int MAX = 7;
+
+    public static void main(String[] args) {
+        System.out.println(sumOfRange(MIN, MAX));
+    }
+
+    public static int sumOfRange(int min, int max) {
+        int sum = 0;
+
+        if (min > max) {
+            sum = 0;
+        } else {
+            for (int i = min; i <= max; i++) {
+                sum += i;
+            }
+            return sum;
+        }
+
+        return sum;
+    }
 }
