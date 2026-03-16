5e35f7c0025e509c0374a24adfd5f0ffa5df443f
Stephen Handy <K1ngHandy@users.noreply.github.com>
2026-03-16 19:06:34 -0400

    add xyz

---
+++ week9/XYZ.java
@@ -0,0 +1,14 @@
+package week9;
+
+public class XYZ {
+    public static void main(String[] args) {
+        int n = 23;
+
+        if (n <= 5)
+            if (n >= 2)
+                System.out.println("x");
+            else
+                System.out.println("y");
+        System.out.println("z");
+    }
+}
