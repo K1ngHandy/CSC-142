88cd9ba5d541957d64c118817497fc7f30142fb3
Stephen Handy <K1ngHandy@users.noreply.github.com>
2026-03-15 23:39:27 -0400

    calc pay

--- week7/printPay.java
+++ week7/printPay.java
@@ -1,15 +1,25 @@
 package week7;
 
 public class PrintPay {
-    public static final double SALARY = 10.0;
-    public static final int HOURS = 40;
+    public static final double SALARY = 10.00;
+    public static final int HOURS = 11;
 
     public static void main(String[] args) {
         printPay(SALARY, HOURS);
     }
 
-    public static int printPay(double salary, int hours) {
+    public static double printPay(double salary, int hours) {
+        double pay;
+
+        if (hours > 8) {
+            pay = salary * 8 + (hours - 8) * salary * 1.5;
+        } else {
+            pay = salary * hours;
+        }
+
         System.out.println("Hours worked: " + hours);
-        System.out.print("Pay Earned: " + salary * hours);
+        System.out.printf("Pay earned: $%.2f%n", pay);
+
+        return pay;
     }
 }
