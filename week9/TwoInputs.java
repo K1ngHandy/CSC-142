21145553d38252af27774863c11de5f49e1a7a05
Stephen Handy <K1ngHandy@users.noreply.github.com>
2026-03-16 19:06:54 -0400

    2 inputs

--- week9/TwoInputs.java
+++ week9/TwoInputs.java
@@ -3,7 +3,7 @@ package week9;
 import java.util.Scanner;
 
 public class TwoInputs {
-    // Input 2 nums and calculate product
+    // Input 2 numbers to calculate product
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         System.out.print("Please type 2 numbers: ");
