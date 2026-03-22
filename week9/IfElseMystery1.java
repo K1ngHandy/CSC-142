40fca24d72be41e6fbcfd5194bf40f3dc5cd796b
Stephen Handy <K1ngHandy@users.noreply.github.com>
2026-03-22 18:51:54 -0400

    mystery 1

--- week9/IfElseMystery1.java
+++ week9/IfElseMystery1.java
@@ -1,5 +1,23 @@
 package week9;
 
 public class IfElseMystery1 {
-    
+    public static void main(String[] args) {
+        mystery(8);
+        mystery(-3);
+        mystery(1);
+        mystery(0);
+    }
+
+    public static void mystery(int n) {
+        System.out.print(n + " ");
+        if (n > 0) {
+            n = n - 5;
+        }
+        if (n < 0) {
+            n = n + 7;
+        } else {
+            n = n * 2;
+        }
+        System.out.println(n);
+    }
 }
