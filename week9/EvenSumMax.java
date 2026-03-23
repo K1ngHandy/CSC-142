0ea94325afc593e9020430ed044afb139c143b47
Stephen Handy <K1ngHandy@users.noreply.github.com>
2026-03-23 18:45:08 -0400

    add

--- week9/EvenSumMax.java
+++ week9/EvenSumMax.java
@@ -4,17 +4,32 @@ import java.util.Scanner;
 
 public class EvenSumMax {
     public static void main(String[] args) {
-        System.out.print("Input integers to sum: ");
         Scanner console = new Scanner(System.in);
-        int nums = console.nextInt();
-        evenSumMax(nums);
+        evenSumMax(console);
     }
 
-    public static int evenSumMax(int nums) {
-        int n = nums.length;
+    public static void evenSumMax(Scanner console) {
+        System.out.print("how many integers? ");
+        int count = console.nextInt();
 
-        for (int i = 1; i <= n.length; i++) {
-            System.out.print("Number: " + n);
+        int evenSum = 0;
+        int max = 0;
+        boolean isEven = false;
+
+        for (int i = 0; i < count; i++) {
+            System.out.print("next integer? ");
+            int num = console.nextInt();
+
+            if (num % 2 == 0) {
+                evenSum += num;
+                if (!isEven || num > max) {
+                    max = num;
+                }
+                isEven = true;
+            }
         }
+
+        System.out.println(" even sum = " + evenSum);
+        System.out.println("even max = " + (isEven ? max : "none"));
     }
 }
