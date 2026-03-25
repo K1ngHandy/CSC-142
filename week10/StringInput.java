32dafced38f4519673b1cd1e10e0bc610be8ad40
Stephen Handy <K1ngHandy@users.noreply.github.com>
2026-03-25 18:57:27 -0400

    add comments

--- week10/StringInput.java
+++ week10/StringInput.java
@@ -6,7 +6,9 @@ public class StringInput {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         System.out.print("What is your name? ");
+        // capture the first word of the input
         String name = sc.next();
+        // or equals()
         if (name.equalsIgnoreCase("Olivia")) {
             System.out.println("My name is Olivia,");
             System.out.println("Nice to meet you!");
