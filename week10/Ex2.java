a7a3ebd080fe01eb518fce3691537be4a21a922e
Stephen Handy <K1ngHandy@users.noreply.github.com>
2026-03-25 18:22:08 -0400

    example 2

---
+++ week10/Ex2.java
@@ -0,0 +1,15 @@
+package week10;
+
+public class Ex2 {
+    public static void main(String[] args) {
+        String s1 = "Olivia Scott";
+        String s2 = "Isabella Scott";
+
+        System.out.println(s2.length());
+        System.out.println(s1.indexOf("e"));
+        System.out.println(s2.indexOf("e"));
+        System.out.println(s1.substring(7, 10));
+        System.out.println(s2.substring(4));
+        System.out.println(s2.toLowerCase());
+    }
+}
