a07740ac1ee686f2ad5666cb61d49b4bd8e047da
Stephen Handy <K1ngHandy@users.noreply.github.com>
2026-03-27 23:31:59 -0400

    add files

---
+++ week10/AddCommas.java
@@ -0,0 +1,11 @@
+package week10;
+
+public class AddCommas {
+    public static void main(String[] args) {
+        addCommas("12345678");
+    }
+
+    public static void addCommas(String number) {
+
+    }
+}
