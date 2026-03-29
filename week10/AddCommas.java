43a9f85c9fd6d38d6e3eb4acaedbab5c512ca7a0
Stephen Handy <K1ngHandy@users.noreply.github.com>
2026-03-29 13:59:50 -0400

    input num string

--- week10/AddCommas.java
+++ week10/AddCommas.java
@@ -1,11 +1,17 @@
 package week10;
 
+import java.util.Scanner;
+
 public class AddCommas {
     public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Input number string: ");
+        String number = sc.next();
+
         addCommas("12345678");
     }
 
     public static void addCommas(String number) {
-
+        System.out.println(number.substring(number.length() - 3));
     }
 }
