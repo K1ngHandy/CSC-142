6e927524deab5751e695348bb01e5e6741653f82
Stephen Handy <K1ngHandy@users.noreply.github.com>
2026-04-01 18:47:31 -0400

    move comments

--- week11/ReadAFile.java
+++ week11/ReadAFile.java
@@ -6,15 +6,15 @@ import java.io.FileNotFoundException;
 
 public class ReadAFile {
     public static void main(String[] args) {
-        // Scanner sc = new Scanner(newFile("in.txt"));
-        // same, condensed
-
         try {
+            // Scanner sc = new Scanner(newFile("in.txt"));
+            // same, condensed
             File f = new File("in.txt");
             Scanner sc = new Scanner(f);
             String text = sc.nextLine();
             System.out.println(text);
         } catch (FileNotFoundException e) {
+            // handle the exception
             System.out.println("File not found");
         }
     }
