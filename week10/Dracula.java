62a00296d26dbcd8037d0f7aee9877a4068264cf
Stephen Handy <K1ngHandy@users.noreply.github.com>
2026-04-06 16:53:07 -0400

    substrings

--- week10/Dracula.java
+++ week10/Dracula.java
@@ -1,5 +1,13 @@
 package week10;
 
 public class Dracula {
-    
+    public static void main(String[] args) {
+        String s = "dracula";
+        for (int i = 0; i < s.length(); i++) {
+            char a = s.charAt(0);
+            String b = s.substring(1);
+            s = b + a;
+        }
+        System.out.println(s);
+    }
 }
