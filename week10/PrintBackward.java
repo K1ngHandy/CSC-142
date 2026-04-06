1311b26e17a055fba4d44741d82f9cb99d9f07b9
Stephen Handy <K1ngHandy@users.noreply.github.com>
2026-04-06 18:51:19 -0400

    print

--- week10/PrintBackward.java
+++ week10/PrintBackward.java
@@ -1,5 +1,21 @@
 package week10;
 
+import java.util.Scanner;
+
 public class PrintBackward {
-    
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter string to print backwards: ");
+        String forward = sc.nextLine();
+
+        printBackward(forward);
+    }
+
+    public static void printBackward(String forward) {
+        //
+        for (int i = forward.length() - 1; i >= 0; i--) {
+            System.out.print(forward.charAt(i));
+        }
+        System.out.println();
+    }
 }
