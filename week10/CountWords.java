c1bd32b918dacf504d2ec972d3ba183476230fc8
Stephen Handy <K1ngHandy@users.noreply.github.com>
2026-04-06 15:06:21 -0400

    count words

--- week10/CountWords.java
+++ week10/CountWords.java
@@ -1,5 +1,26 @@
 package week10;
 
+import java.util.Scanner;
+
 public class CountWords {
-    
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter string for word count: ");
+        String strInput = sc.nextLine();
+
+        System.out.println(countWords(strInput));
+
+    }
+
+    public static int countWords(String strInput) {
+        int count = 0;
+
+        if (strInput.isEmpty()) {
+            return count;
+        } else {
+            String regex = "\\s+";
+            count = strInput.trim().split(regex).length;
+            return count;
+        }
+    }
 }
