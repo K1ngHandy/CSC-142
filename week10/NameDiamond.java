c01863b69a5e61a07b64ed3954d71dfe67a982d7
Stephen Handy <K1ngHandy@users.noreply.github.com>
2026-04-06 17:45:38 -0400

    name diamond

--- week10/NameDiamond.java
+++ week10/NameDiamond.java
@@ -1,5 +1,27 @@
 package week10;
 
+import java.util.Scanner;
+
 public class NameDiamond {
-    
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Input string to convert to diamond: ");
+        String str = sc.nextLine();
+
+        nameDiamond(str);
+    }
+
+    public static void nameDiamond(String str) {
+        // top
+        for (int i = 1; i <= str.length(); i++) {
+            System.out.println(str.substring(0, i));
+        }
+        // bottom
+        for (int i = 1; i < str.length(); i++) {
+            // repeats " " at start of line i times, then prints substring from i to end of
+            // string
+            System.out.println(" ".repeat(i) + str.substring(i));
+        }
+        System.out.println();
+    }
 }
