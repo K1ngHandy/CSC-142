72e2659e2f7a4361613c0fa5e5a7cc16eedaa0b6
Stephen Handy <K1ngHandy@users.noreply.github.com>
2026-04-05 23:21:01 -0400

    characters in string

--- week10/ContainsTwice.java
+++ week10/ContainsTwice.java
@@ -1,5 +1,28 @@
 package week10;
 
+import java.util.Scanner;
+
 public class ContainsTwice {
-    
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter string, then character: ");
+        String str = sc.next();
+        char character = sc.next().charAt(0);
+
+        System.out.println(containsTwice(str, character));
+    }
+
+    public static boolean containsTwice(String str, char character) {
+        int count = 0;
+        for (int i = 0; i < str.length(); i++) {
+            if (str.charAt(i) == character) {
+                count++;
+            }
+
+            if (count >= 2) {
+                return true;
+            }
+        }
+        return false;
+    }
 }
