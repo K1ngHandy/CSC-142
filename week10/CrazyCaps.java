cee23ce0ceb9ba9270c246ee77dfc4b29be2fb45
Stephen Handy <K1ngHandy@users.noreply.github.com>
2026-04-06 16:39:11 -0400

    crazy caps

--- week10/CrazyCaps.java
+++ week10/CrazyCaps.java
@@ -1,5 +1,28 @@
 package week10;
 
+import java.util.Scanner;
+
 public class CrazyCaps {
-    
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Input string for crazy caps: ");
+        String inputStr = sc.nextLine();
+
+        System.out.print(crazyCaps(inputStr));
+    }
+
+    public static String crazyCaps(String inputStr) {
+        StringBuilder res = new StringBuilder();
+
+        for (int i = 0; i < inputStr.length(); i++) {
+            char current = inputStr.charAt(i);
+
+            if (i % 2 == 0) {
+                res.append(Character.toLowerCase(current)); // even
+            } else {
+                res.append(Character.toUpperCase(current)); // odd
+            }
+        }
+        return res.toString();
+    }
 }
