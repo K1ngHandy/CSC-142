ddfa2b74a7b7a72e50ef5db9b8e0d711f6dc3b9c
Stephen Handy <K1ngHandy@users.noreply.github.com>
2026-04-10 13:35:32 -0400

    is vowels check

--- week10/IsAllVowels.java
+++ week10/IsAllVowels.java
@@ -9,12 +9,21 @@ public class IsAllVowels {
         String input = sc.nextLine();
 
         isAllVowels(input);
+
+        if (isAllVowels(input)) {
+            System.out.println("All characters are vowels.");
+        } else {
+            System.out.println("Not all vowels.");
+        }
     }
 
-    public static void isAllVowels(String input) {
+    public static boolean isAllVowels(String input) {
         for (int i = 0; i < input.length(); i++) {
-            //
+            char ch = Character.toLowerCase(input.charAt(i));
+            if (ch != 'a' && ch != 'e' && ch != 'i' && ch != 'o' && ch != 'u') {
+                return false;
+            }
         }
-        System.out.print(input);
+        return true;
     }
 }
