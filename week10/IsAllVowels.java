24fe00d44181f1bf1457acceb002dbdf0bd12ce4
Stephen Handy <K1ngHandy@users.noreply.github.com>
2026-04-10 13:17:11 -0400

    all vowels structure

--- week10/IsAllVowels.java
+++ week10/IsAllVowels.java
@@ -1,5 +1,20 @@
 package week10;
 
+import java.util.Scanner;
+
 public class IsAllVowels {
-    
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter a string: ");
+        String input = sc.nextLine();
+
+        isAllVowels(input);
+    }
+
+    public static void isAllVowels(String input) {
+        for (int i = 0; i < input.length(); i++) {
+            //
+        }
+        System.out.print(input);
+    }
 }
