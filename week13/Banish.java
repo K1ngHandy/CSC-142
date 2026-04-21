11eed7860b2d485e2a07fa31cd1c028f5cba5e21
Stephen Handy <K1ngHandy@users.noreply.github.com>
2026-04-21 10:36:48 -0400

    add banish file

---
+++ week13/Banish.java
@@ -0,0 +1,14 @@
+package week13;
+
+public class Banish {
+    public static void main(String[] args) {
+        int[] a1 = { 42, 3, 9, 42, 42, 0, 42, 9, 42, 42, 17, 8, 2222, 4, 9, 0, 1 };
+        int[] a2 = { 42, 2222, 9 };
+
+        banish(a1, a2);
+    }
+
+    public static void banish(int[] a1, int[] a2) {
+        //
+    }
+}
