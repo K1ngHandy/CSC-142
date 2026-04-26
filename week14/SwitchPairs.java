e85cb34077513458a28fed0a4fc9bb17c5e9fa4b
Stephen Handy <K1ngHandy@users.noreply.github.com>
2026-04-26 12:11:02 -0400

    switch pairs framework

---
+++ week14/SwitchPairs.java
@@ -0,0 +1,13 @@
+package week14;
+
+public class SwitchPairs {
+    public static void main(String[] args) {
+        String[] a = { "a", "bb", "c", "ddd", "ee", "f", "g" };
+
+        switchPairs(a);
+    }
+
+    public static String[] switchPairs() {
+        //
+    }
+}
