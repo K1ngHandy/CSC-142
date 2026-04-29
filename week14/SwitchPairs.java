9c0b5f62ccef61a870f8239d54e7b723fdc5ed5a
Stephen Handy <K1ngHandy@users.noreply.github.com>
2026-04-29 00:16:06 -0400

    pair swap

--- week14/SwitchPairs.java
+++ week14/SwitchPairs.java
@@ -1,20 +1,21 @@
 package week14;
 
+import java.util.Arrays;
+
 public class SwitchPairs {
     public static void main(String[] args) {
         String[] a = { "a", "bb", "c", "ddd", "ee", "f", "g" };
 
-        switchPairs(a);
+        System.out.println(Arrays.toString(switchPairs(a)));
     }
 
-    public static int[] switchPairs() {
-        int[] switch = new String[a.length];
-
-        for (int i = 0; i < a.length; i++) {
-            switch[i] = a[i + 1];
-            switch[i + 1] = a[i];
+    public static String[] switchPairs(String[] a) {
+        for (int i = 0; i < a.length - 1; i += 2) {
+            String temp = a[i];
+            a[i] = a[i + 1];
+            a[i + 1] = temp;
         }
 
-        return switch
+        return a;
     }
 }
