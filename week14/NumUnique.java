04d0565c7465a88582b3e90d853a4ae533d7d862
Stephen Handy <K1ngHandy@users.noreply.github.com>
2026-04-30 09:49:46 -0400

    scaffolding

---
+++ week14/NumUnique.java
@@ -0,0 +1,15 @@
+package week14;
+
+public class NumUnique {
+    public static void main(String[] args) {
+        int[] list = { 5, 7, 7, 7, 8, 22, 22, 23, 31, 35, 35, 40, 40, 40, 41 };
+
+        numUnique(list);
+    }
+
+    public static void numUnique(int[] list) {
+        for (int i = 0; i < list.length - 1; i += 2) {
+            //
+        }
+    }
+}
--- week14/PromptNumbers2.java
+++ week14/PromptNumbers2.java
@@ -1,5 +1,30 @@
 package week14;
 
-public class PromptNumbers2 {
-    
+public class PromptNumbers {
+    public static void main(String[] args) {
+        Scanner console = new Scanner(System.in);
+        System.out.print("How many numbers will you enter? ");
+        int count = console.nextInt(console);
+
+        for (int i = 0; i < ???; i++) {
+            // your code goes here; store the numbers
+            System.out.print("Type a number: ");
+            int[] arr = {};
+            return arr;
+        }
+
+        System.out.println();
+        System.out.println("Your numbers in forward order:");
+
+        for (int i = 0; i < arr.length; i++) {
+            // your code goes here; print the numbers in forward order
+            System.out.print(arr[i]);
+        }
+
+        System.out.println();
+        System.out.println("Your numbers in backward order:");
+
+        // your code goes here; print the numbers in forward order
+
+    }
 }
