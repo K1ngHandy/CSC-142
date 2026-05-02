08a74f5176380c37d8f01cd0842d833bfe6d03c2
Stephen Handy <K1ngHandy@users.noreply.github.com>
2026-05-02 12:17:34 -0400

    n copies scaffolding

---
+++ week14/NCopies.java
@@ -0,0 +1,17 @@
+package week14;
+
+public class NCopies {
+    public static void main(String[] args) {
+        int[] a = { 3, 5, 0, 2, 2, -7, 0, 4 };
+
+        nCopies(a, n);
+    }
+
+    public static void nCopies(int[] a, int n) {
+        for (int i = 0; i < a.length; i++) {
+            for (int copy = 0; copy < n; copy++) {
+                //
+            }
+        }
+    }
+}
