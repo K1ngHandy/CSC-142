4ee9277e6d665e72ae921ee6306633775e7254cc
Stephen Handy <K1ngHandy@users.noreply.github.com>
2026-05-06 18:06:58 -0400

    add points

--- week15/Point.java
+++ week15/Point.java
@@ -38,5 +38,17 @@ public class Point {
     public static void main(String[] args) {
         Point p = new Point(4, 3);
         System.out.println(p.toString());
+
+        Point p1 = new Point(17, 9);
+        Point p2 = new Point(4, -1);
+        Point p3 = p2;
+
+        p1.translate(3, 1);
+        p2.x = 50;
+        p3.translate(-4, 5);
+
+        System.out.println(p1.toString());
+        System.out.println(p2.toString());
+        System.out.println(p3.toString());
     }
 }
